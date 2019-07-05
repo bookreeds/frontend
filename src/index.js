1e737508afe5ca9196e47401457183bf69ee3bd5
Sayan Chakraborty <mail.sayanc@gmail.com>
2019-07-06 04:27:56 +0530

    Fix linting configurations

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home.js";
 import Navbar from "./components/Navbar.js";
 import NotFound from "./components/NotFound.js";
-import * as serviceWorker from './serviceWorker';
-
+import * as serviceWorker from "./serviceWorker";
 
 const App = () => {
-  return (
-    <Router>
-      <Navbar>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </Navbar>
-    </Router>
-  )
-}
+	return (
+		<Router>
+			<Navbar>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route component={NotFound} />
+				</Switch>
+			</Navbar>
+		</Router>
+	);
+};
 
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
